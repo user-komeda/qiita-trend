ccec1a2961202daf5533473d1de60930cac4c7ef
user-komeda <shigoto922@gmail.com>
2024-11-26 01:11:29 +0900

    #117 testファイルもlint対象にする

--- src/app/const/const.ts
+++
@@ -1,6 +0,0 @@
-/** DRAWER_WIDTH */
-export const DRAWER_WIDTH = 300
-/** BASE_URL */
-export const BASE_URL = 'http://localhost:3200'
-/** GET_ALL_ITEM_API_URL */
-export const GET_ALL_ITEM_API_URL = '/public/items/'
